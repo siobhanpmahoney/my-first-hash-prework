11a6384e470e0ec44e7c45bac15851a9b16e80bd
Siobhan Mahoney <siobhan.p.mahoney@gmail.com>
2017-10-27 23:14:32 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,10 +4,10 @@ end
 
 
 def shipping_manifest
-  shopping_manifest = {}
-  shopping_manifest["whale bone corsets"] = 5
-  shopping_manifest["porcelain vases"] = 2
-  shopping_manifest["oil paintings"] = 3
+  shipping_manifest = {}
+  shipping_manifest["whale bone corsets"] = 5
+  shipping_manifest["porcelain vases"] = 2
+  shipping_manifest["oil paintings"] = 3
 end
 
 def retrieval
