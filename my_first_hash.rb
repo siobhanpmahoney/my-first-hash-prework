6d445c7e9a79bc9b06284987c9b982eb35f96f02
Siobhan Mahoney <siobhan.p.mahoney@gmail.com>
2017-10-27 23:26:10 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -15,4 +15,5 @@ end
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
   shipping_manifest["pearl necklace"] = 1
+  shipping_manifest
 end
